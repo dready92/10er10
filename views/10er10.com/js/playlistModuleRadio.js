ce5ef48ab68973c428c166a25604fd0f207f9555
Michael BAILLY <mbailly@mbailly-2B4V0N1.(none)>
2011-05-16 18:37:06 +0200

    indentation

--- views/10er10.com/js/playlistModuleRadio.js
+++ views/10er10.com/js/playlistModuleRadio.js
@@ -5,7 +5,6 @@ var createModule= function (ui) {
 
 	var overlay,
 	delayTimeout = null
-
 	;
 
 	var appendRandomSongs = function(count, genres) {
@@ -42,69 +41,72 @@ var createModule= function (ui) {
 	};
 
 	var module = new d10.fn.playlistModule("radio", {
-                "playlist:currentSongChanged": function(e) {
-                        if ( delayTimeout ) {
-                                clearTimeout(delayTimeout);
-                        }
-                        delayTimeout = setTimeout(function() {
-                                if ( ui.find(".autofill").hasClass("enabled") && d10.playlist.current().nextAll().length < 3 ) {
-                                        debug("radio2");
-                                        appendSongs(settings.count);
-                                }
-                        }, settings.delay);
-                }
-        }, {
+		"playlist:currentSongChanged": function(e) {
+				if ( delayTimeout ) {
+					clearTimeout(delayTimeout);
+				}
+				delayTimeout = setTimeout(function() {
+					if ( ui.find(".autofill").hasClass("enabled") && d10.playlist.current().nextAll().length < 3 ) {
+						debug("radio2");
+						appendSongs(settings.count);
+					}
+				}, settings.delay);
+		}
+	}, {
 		enable: function() {ui.css({display:"block"});return this;},
 		disable: function() {ui.css({display:"none"});return this;}
 	});
 
 
-		overlay = ui.find("div.overlay");
-		//debug(ui);
-		ui.find(".off > .link").click(function() {
-			if ( !module.isEnabled() ) { return ;}
-			debug("click");
-			$(this).closest(".autofill").addClass("enabled");
-		});
-		ui.find(".on > .link").click(function() {
-			if ( !module.isEnabled() ) { return ;}
-			if ( overlay.ovlay() ) {
-				return overlay.ovlay().close();
-			}
-			var pos = $(this).position();
-			var top = pos.top-200;
-			if ( top < 10 )  top = 10;
-			overlay.css({"top": top ,"left": pos.left-270, "width": "250px"})
-			.ovlay({"load":true});
-		});
-		$("div.disable",overlay).click(function () {
-			if ( !module.isEnabled() ) { return ;}
-			overlay.ovlay().close();
-			ui.find(".autofill").removeClass("enabled");
+	overlay = ui.find("div.overlay");
+	//debug(ui);
+	ui.find(".off > .link").click(function() {
+		if ( !module.isEnabled() ) { return ;}
+		debug("click");
+		$(this).closest(".autofill").addClass("enabled");
+	});
+	ui.find(".on > .link").click(function() {
+		if ( !module.isEnabled() ) { return ;}
+		if ( overlay.ovlay() ) {
+			return overlay.ovlay().close();
+		}
+		var pos = $(this).position();
+		var top = pos.top-200;
+		if ( top < 10 )  top = 10;
+		overlay.css({"top": top ,"left": pos.left-270, "width": "250px"})
+		.ovlay({"load":true});
+	});
+	$("div.disable",overlay).click(function () {
+		if ( !module.isEnabled() ) { return ;}
+		overlay.ovlay().close();
+		ui.find(".autofill").removeClass("enabled");
 // 			ui.find(".off").show();
-		});
-		$("div.close",overlay).click(function() {
-			if ( !module.isEnabled() ) { return ;}
-			overlay.ovlay().close();
-		});
-		$(".list > div",overlay).click(function() {
-			if ( !module.isEnabled() ) { return ;}
- 			$(this).toggleClass("checked"); 
-		});
-		
-		return module;
+	});
+	$("div.close",overlay).click(function() {
+		if ( !module.isEnabled() ) { return ;}
+		overlay.ovlay().close();
+	});
+	$(".list > div",overlay).click(function() {
+		if ( !module.isEnabled() ) { return ;}
+		$(this).toggleClass("checked"); 
+	});
+	
+	return module;
 };
 var settings ;
 
 	
-	settings = $.extend(
-		{
-			delay: 7000,
-			count: 5
-		}
-		
-	,{});
+settings = $.extend(
+	{
+		delay: 7000,
+		count: 5
+	}
+	
+,{});
 
-	var mod = createModule($("#side"));
-	d10.playlist.modules[mod.name] = mod;
+var mod = createModule($("#side"));
+d10.playlist.modules[mod.name] = mod;
+	
+	
+	
 });
