1449e9e6d4b11508d25b52b9342a34a40ddddc3c
Mickael <mickael@dread.(none)>
2011-10-25 23:36:02 +0200

    install/update script fixing title/artist/album case

---
+++ node/install/songFieldsCase.js
@@ -0,0 +1,59 @@
+var fixDoc = function(doc, couchd10, ucwords) {
+	var title = ucwords(doc.title),
+		artist = ucwords(doc.artist),
+		album = ucwords(doc.album);
+	if ( title != doc.title || artist != doc.artist || album != doc.album ) {
+		doc.title = title;
+		doc.artist = artist;
+		doc.album = album;
+		return function(then) {
+			couchd10.storeDoc(doc, function(err,resp) {
+				if ( err ) {
+					console.log("Warning: unable to fix case for song ",doc,err,resp);
+				}
+				then();
+			});
+		};
+	}
+};
+
+
+exports.fixSongFieldsCase = function(couchd10, ucwords, then) {
+	couchd10.getAllDocs(
+		{
+			startkey: "aa",
+			endkey: "ab",
+			inclusive_end: false,
+			include_docs: true
+		},
+		function(err,resp) {
+			if ( err ) {
+				console.log("ERROR: can't get songs list from database: ",err,resp);
+				return ;
+			}
+			var toFix = [];
+			resp.rows.forEach(function(v,k) {
+				var fix = fixDoc(v.doc, couchd10, ucwords);
+				if(fix) {
+					toFix.push(fix);
+				}
+			});
+			if ( toFix.length ) {
+				console.log("got ",toFix.length," songs to fix");
+				var fixNext = function() {
+					if ( toFix.length ) {
+						var fix = toFix.pop();
+						fix(fixNext);
+					} else {
+						return then();
+					}
+				};
+				fixNext();
+			} else {
+				return then();
+			}
+		}
+	);
+	
+	
+};
