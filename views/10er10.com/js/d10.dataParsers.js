b40ffa6bc600d799e214f5f976b5dc8ac2b42c03
Mickael Bailly <mickael.bailly@free.fr>
2011-11-30 17:31:17 +0100

    fix: correctly encode album names in URLs

--- views/10er10.com/js/d10.dataParsers.js
+++ views/10er10.com/js/d10.dataParsers.js
@@ -1,6 +1,7 @@
 define(["js/d10.templates", "js/d10.utils", "js/d10.imageUtils"], function(tpl, toolbox, imageUtils) {
 	function singleAlbumParser(songs) {
-		var albumData = {duration: 0, songsNb: songs.length, artists: [], genres: [], image_class: [], songs: ""}, artists = {}, genres = {}, duration = 0, images = {};
+		var albumData = {duration: 0, songsNb: songs.length, artists: [], genres: [], image_class: [], songs: "", e_artists: [], e_genres: []}, 
+			artists = {}, genres = {}, duration = 0, images = {};
 		songs.forEach(function(row) {
 			albumData.album = row.doc.album;
 			artists[row.doc.artist] = 1;
@@ -19,11 +20,20 @@ define(["js/d10.templates", "js/d10.utils", "js/d10.imageUtils"], function(tpl, 
 			}
 			albumData.songs+=tpl.song_template(row.doc);
 		});
-		for ( var k in artists ) { albumData.artists.push(k); }
-		for ( var k in genres ) { albumData.genres.push(k); }
+		for ( var k in artists ) {
+			var e = encodeURIComponent(k);
+			albumData.artists.push({name: k, encoded: e});
+			albumData.e_artists.push( encodeURIComponent( k ) );
+		}
+		for ( var k in genres ) {
+			var e = encodeURIComponent(k);
+			albumData.genres.push({name: k, encoded: e}); 
+			albumData.e_genres.push( encodeURIComponent( k ) );
+		}
 		var d = new Date(1970,1,1,0,0,duration);
 		albumData.duration = d.getMinutes()+':'+d.getSeconds();
 		albumData.image_url = toolbox.keyOfHighestValue(images);
+		albumData.e_album = encodeURIComponent(albumData.album);
 		if ( albumData.image_url ) { 
 			albumData.image_url = imageUtils.getImageUrl(albumData.image_url);
 		} else {
