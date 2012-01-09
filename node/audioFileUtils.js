49d325a50978e15a278f54bd5167fd03eaae8254
Mickael Bailly <mickael.bailly@free.fr>
2012-01-09 16:59:10 +0100

    remove commented code

--- node/audioFileUtils.js
+++ node/audioFileUtils.js
@@ -78,92 +78,9 @@ exports.id3tags = function(file,cb) {
 		}
 	});
 };
-/*
-exports.id3tags = function(file, cb, secondTry) {
-	var utrac_opts = " -t UTF-8";
-	if ( secondTry ) {
-		utrac_opts += " -f LATIN1";
-	}
-	var tagsPipe = exec(d10.config.cmds.taginfo+" "+file+" | "+d10.config.cmds.utrac+utrac_opts,{maxBuffer: 2000*1024},function(err,stdout,stderr) {
-		if ( err )	{
-			if ( err.message.indexOf("error 303") > -1 ) {
-				return cb(null,{});
-			} else if ( !secondTry && err.message.indexOf("Command failed: Segmentation fault") > -1 ) {
-				return exports.id3tags(file,cb,true);
-			}
-			return cb(null,{});
-		}
-		var tagnames = ['ALBUM','TRACK','ARTIST','TITLE','GENRE','YEAR'],
-			tags = {};
-		stdout.split("\n").forEach(function(v,k) {
-			var tag = v.split("=",2);
-			if ( tagnames.indexOf(tag[0]) >=0 ) {
-				tags[tag[0]] = tag[1].replace(/^\s+/,"").replace(/\s+$/,"").replace(/"/g,"");
-				if ( tag[0] == "TRACK" ) {
-					tags["TRACKNUMBER"] = tags[tag[0]];
-				} else if ( tag[0] == "YEAR" ) {
-					tags["DATE"] = tags[tag[0]];
-				}
-			}
-		});
-		d10.log("debug",tags);
-		cb(null,tags);
-	});
-};
-*/
-/*
-exports.oggtags = function(file, cb) {
-	var tagsPipe = exec(d10.config.cmds.vorbiscomment+" -l "+file+" | "+d10.config.cmds.utrac+" -t UTF-8",{maxBuffer: 2000*1024},function(err,stdout,stderr) {
-		if ( err )	{
-			if ( err.message.indexOf("error 303") > -1 ) {
-				return cb(null,{});
-			}
-			return cb(err);
-		}
-		var tags = {};
-		d10.log("debug","TAGS bruts: ",stdout);
-		stdout.split("\n").forEach(function(v,k) {
-			var tag = v.split("=",2);
-			if ( tag.length > 1 ) {
-				tags[tag[0]] = tag[1].replace(/^\s+/,"").replace(/\s+$/,"").replace(/"/g,"");
-			}
-		});
-		d10.log("debug",tags);
-		cb(null,tags);
-	});
-};
-*/
 
 exports.oggtags = exports.id3tags;
-/*
-exports.flactags = function(file,cb) {
-	d10.log("launching flactags");
-	var tagsPipe = exec(d10.config.cmds.metaflac+" --list "+file+" | grep -F comment[ | "+d10.config.cmds.utrac+" -t UTF-8",{maxBuffer: 2000*1024},function(err,stdout,stderr) {
-		if ( err )	{
-			d10.log("err",err);
-			if ( err.message.indexOf("error 303") > -1 ) {
-				return cb(null,{});
-			}
-			return cb(err);
-		}
-		var tags = {};
-		d10.log("debug","TAGS bruts: ",stdout);
-		stdout.split("\n").forEach(function(v,k) {
-			var tagline = v.split(": ",2);
-			d10.log("tagline: ",tagline);
-			if ( tagline.length == 2 ) {
-				var tag = tagline[1].split("=",2);
-				if ( tag.length > 1 ) {
-					tags[tag[0]] = tag[1].replace(/^\s+/,"").replace(/\s+$/,"").replace(/"/g,"");
-				}
-			}
-		});
-		d10.log("debug","FLAC tags");
-		d10.log("debug",tags);
-		cb(null,tags);
-	});
-};
-*/
+
 exports.flactags = exports.id3tags;
 
 exports.escapeShellArg = function(s) {
