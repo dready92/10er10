36605f425db9d1f8f41a0a21b4923a1f01572aac
Mickael Bailly <mickael.bailly@free.fr>
2012-02-29 19:43:56 +0100

    reviewHelper bugfixes

--- views/10er10.com/js/my.reviewHelper.js
+++ views/10er10.com/js/my.reviewHelper.js
@@ -1,66 +1,69 @@
 "use strict";
 define(["js/d10.templates", "js/user"], function(tpl, user) {
-  
-  	var showReviewHelper = function (reference) {
-	  var reviewHelper = $(tpl.mustacheView("my.reviewHelper.bubble")).css({visibility: "hidden"}).appendTo($("body"));
-	  var reviewHelperBody = $(tpl.mustacheView("my.reviewHelper"));
+    var body = $("body");
+    var reviewReminder = $("#reviewReminder");
+    var reviewHelper = $(tpl.mustacheView("my.reviewHelper.bubble"));
+    var reviewHelperBody = $(tpl.mustacheView("my.reviewHelper"));
+  	var showReviewHelper = function () {
+	  reviewHelper.css({visibility: "hidden"}).appendTo(body);
 	  var startOverlay = function () {
 		reviewHelper.ovlay(
 		  {
 			closeOnClick: false, 
 			closeOnMouseOut: false, 
 			closeOnEsc: false, 
-			align:{position: "top", reference: reference, topOffset: -9}, 
-			family: "reviewHelper"
+			align:{position: "top", reference: reviewReminder, topOffset: -9}, 
+			family: "reviewHelper",
+            onClose: function() {reviewHelper.detach();}
 		  }
 		);
 	  };
-	  
-	  reviewHelper.find(".close").click(function() {
-		reviewHelper.ovlay().close();
-		user.set_preference("hiddenReviewHelper", true);
-	  });
-	  reviewHelper.find(".link").click(function() {
-		if ( reviewHelperBody.ovlay() ) {
-		  reviewHelperBody.ovlay().close();
-		} else {
-		  reviewHelperBody.find(".close").click(function() { reviewHelperBody.ovlay().close(); });
-		  reviewHelperBody.find("button").click(function() { reviewHelperBody.ovlay().close(); });
-		  reviewHelperBody.fullOvlay(
-			{
-			  closeOnClick: true, 
-			  closeOnMouseOut: false, 
-			  closeOnEsc: true, 
-			  align:{position: "bottom", reference: reference}, 
-			  family: "reviewHelper",
-			  onClose: startOverlay,
-			  onDomInsert: function(overlay) {setTimeout(function() {overlay.addClass("on");},20);},
-			  onDomRemove: function(overlay, then) {overlay.removeClass("on"); setTimeout(then,300);}
-			}
-		  );
-		}
-	  });
 	  startOverlay();
 	};
+    
 
+    reviewHelper.find(".close").click(function() {
+      reviewHelper.ovlay().close();
+      user.set_preference("hiddenReviewHelper", true);
+    });
+    reviewHelper.find(".link").click(function() {
+      if ( reviewHelperBody.ovlay() ) { 
+        reviewHelperBody.ovlay().close(); 
+      } else {
+        reviewHelperBody.find(".close").click(function() { reviewHelperBody.ovlay().close(); });
+        reviewHelperBody.find("button").click(function() { reviewHelperBody.ovlay().close(); });
+        reviewHelperBody.fullOvlay(
+          {
+            closeOnClick: true, 
+            closeOnMouseOut: false, 
+            closeOnEsc: true,
+            family: "reviewHelper",
+            onClose: showReviewHelper,
+            onDomInsert: function(overlay) {setTimeout(function() {overlay.addClass("on");},20);},
+            onDomRemove: function(overlay, then) {overlay.removeClass("on"); setTimeout(then,300);}
+          }
+        );
+      }
+    });
+    
 	var onData = function(data) {
+        
 	  	var count = data.count;
-		var rr = $("#reviewReminder");
 		if ( count ) {
-			$("strong",rr).html(count);
-			rr.attr("title",tpl.mustacheView("side.review_reminder",{count: count}));
-			if ( rr.is(":visible") ) {
-				rr.whoobee();
+			reviewReminder.find("strong").html(count);
+			reviewReminder.attr("title",tpl.mustacheView("side.review_reminder",{count: count}));
+			if ( reviewReminder.is(":visible") ) {
+				reviewReminder.whoobee();
 			} else {
-				rr.slideDown(function() {
-					rr.flipflap();
+				reviewReminder.slideDown(function() {
+					reviewReminder.flipflap();
 					if ( !user.get_preferences() || !user.get_preferences().hiddenReviewHelper ) {
-					  setTimeout(function() { showReviewHelper(rr); }, 500);
+					  setTimeout(function() { showReviewHelper(); }, 500);
 					}
 				});
 			}
-		} else if ( rr.is(":visible") ) {
-			rr.slideUp("fast");
+		} else if ( reviewReminder.is(":visible") ) {
+			reviewReminder.slideUp("fast");
             var overlay = reviewHelper.ovlay();
             if ( overlay ) { overlay.close(); }
 		}
