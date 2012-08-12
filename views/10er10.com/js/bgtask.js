76263e4b63e1ab5185fc35a309f803522425aca8
Mickael Bailly <mickael.bailly@free.fr>
2012-08-12 15:34:57 +0200

    correct indentation

--- views/10er10.com/js/bgtask.js
+++ views/10er10.com/js/bgtask.js
@@ -1,66 +1,62 @@
 define(["js/d10.rest", "js/d10.events"],function(rest, pubsub) {
 
-	var bgtask = function() {
-		var ival = null;
+  var bgtask = function() {
+    var ival = null;
 
-		var tasks = [
-			function() {
-			rest.server.load({
-				load: function(err, data) {
-				if ( !err  ) {
-					var load = data.load.shift();
-					if ( load < 3 ) {
-					$("body").data("cache_ttl",15000);
-					} else if ( load < 6 ) {
-					$("body").data("cache_ttl",60000);
-					} else {
-					$("body").data("cache_ttl",300000);
-					}
-				}
-				}
-			});
+    var tasks = [
+      function() {
+        rest.server.load({
+          load: function(err, data) {
+            if ( !err  ) {
+              var load = data.load.shift();
+              if ( load < 3 ) {
+                $("body").data("cache_ttl",15000);
+              } else if ( load < 6 ) {
+                $("body").data("cache_ttl",60000);
+              } else {
+                $("body").data("cache_ttl",300000);
+              }
+            }
+          }
+        });
+      },
+      function() {
+        rest.server.length({
+          load: function(err, data) {
+            if ( !err  ) {
+              var length = parseInt(data.length / 60 / 60);
+              $("footer span.hours").html(length);
+            }
+          }
+        });
+      }
+    ];
 
-			},
-			function() {
-			rest.server.length({
-				load: function(err, data) {
-				if ( !err  ) {
-					var length = parseInt(data.length / 60 / 60);
-					$("footer span.hours").html(length);
-				}
-				}
-			});
+    tasks.unshift(function() {
+      rest.user.review.count({
+        load: function(err, data) {
+          if ( !err  ) {
+            pubsub.topic("review.count").publish(data);
+          }
+        }
+      });
+    });
 
-			}
-		];
+    var run = function () {
+      var tasksCopy = tasks.slice();
+      var runOne = function () { 
+      var task = tasksCopy.pop();
+      task();
+      if ( tasksCopy.length ) { setTimeout(runOne,5000); }
+      }
+      if ( tasksCopy.length ) runOne();
+    };
 
-		tasks.unshift(function() {
-			rest.user.review.count({
-				load: function(err, data) {
-					if ( !err  ) {
-						pubsub.topic("review.count").publish(data);
-					}
-				}
-			});
-		});
+    this.init = function() {
+      run();
+      setInterval(run,150000);
+    };
 
-		var run = function () {
-			var tasksCopy = tasks.slice();
-			var runOne = function () { 
-			var task = tasksCopy.pop();
-			task();
-			if ( tasksCopy.length ) { setTimeout(runOne,5000); }
-			}
-			if ( tasksCopy.length ) runOne();
-		};
-
-
-		this.init = function() {
-			run();
-			setInterval(run,150000);
-		};
-
-	};
-	
-	return new bgtask();
+  };
+  return new bgtask();
 });
