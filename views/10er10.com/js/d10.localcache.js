dbdc3682e83218d9c2aa184b39499c2c4d60aa85
Mickael Bailly <mickael.bailly@free.fr>
2012-08-12 15:30:42 +0200

    fix indentation

--- views/10er10.com/js/d10.localcache.js
+++ views/10er10.com/js/d10.localcache.js
@@ -3,14 +3,14 @@ define(function() {
 var storagebase = {
   getTemplate: function (key) {
     var obj = this.getJSON('site.templates');
-		if ( !obj )	obj = {};
-		return obj[key];
+    if ( !obj )	obj = {};
+    return obj[key];
   },
   setTemplate: function (key,val) {
     var obj = this.getJSON('site.templates');
-		if ( !obj )	obj = {};
-		obj[key] = val;
-		return this.setJSON('site.templates',obj);
+    if ( !obj )	obj = {};
+    obj[key] = val;
+    return this.setJSON('site.templates',obj);
   },
   unsetTemplates: function () {
     return this.unset('site.templates');
@@ -19,24 +19,23 @@ var storagebase = {
   getJSON: function (key) {
     var value = this.get(key);
     if ( value === null || value == undefined ) return value;
-		var val = JSON.parse(value);
-		if ( val && val._cache_ttl ) {
- 			var now = new Date();
-			if ( now.getTime() - val._cache_ttl > $('body').data('cache_ttl') ) {
-				debug("localcache: ttl expired");
-				this.unset(key);
-				return ;
-			}
-			delete val._cache_ttl;
-		}
+    var val = JSON.parse(value);
+    if ( val && val._cache_ttl ) {
+      var now = new Date();
+      if ( now.getTime() - val._cache_ttl > $('body').data('cache_ttl') ) {
+        debug("localcache: ttl expired");
+        this.unset(key);
+        return ;
+      }
+      delete val._cache_ttl;
+    }
     return val;
   },
   setJSON: function (key,val,use_ttl) {
-		if ( use_ttl ) {
-// 			debug("localcache: should use ttl");
+    if ( use_ttl ) {
       now = new Date();
-			val._cache_ttl = now.getTime();
-		}
+      val._cache_ttl = now.getTime();
+    }
     return this.set(key,JSON.stringify(val));
   },
   unsetJSON: function (key) {
@@ -65,13 +64,13 @@ function memStorage () {
   }
   
   this.dump = function () {
-	  var back = {"total": {"items": 0,"bytes":0}, "items": []};
-	  for ( var index in cache ) {
-		  back.total.items++;
-		  back.total.bytes += cache[index].length;
-		  back.items.push ({ "key": index, "bytes": cache[index].length });
-	  }
-	  return back;
+    var back = {"total": {"items": 0,"bytes":0}, "items": []};
+    for ( var index in cache ) {
+      back.total.items++;
+      back.total.bytes += cache[index].length;
+      back.items.push ({ "key": index, "bytes": cache[index].length });
+    }
+    return back;
   };
 
 }
@@ -96,10 +95,11 @@ function localcache ( ) {
   }
 
   this.dump = function () {
-	  return storage.dump();
+    return storage.dump();
   };
 
 }
-	return new localcache();
+
+return new localcache();
 
 });
