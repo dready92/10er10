b8c3590208f78ef513e9348fe8bacba00cc1bfef
Mickael Bailly <mickael.bailly@free.fr>
2012-08-12 15:20:18 +0200

    use Date.now() if it exists

--- views/10er10.com/js/d10.toolbox.js
+++ views/10er10.com/js/d10.toolbox.js
@@ -10,15 +10,17 @@ define(function() {
 		debug("using JS collation");
 		return String.fromCharCode( (l.charCodeAt(0)+1) );
 	};
+    
+  var microtime  = ( "now" in Date ) ?
+    function() { return Date.now() / 1000 } :
+    function() { return new Date().getTime() / 1000 };
+    
 	return {
 		isValidEmailAddress: function (emailAddress) {
 			return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/.test(emailAddress);
 		},
-		microtime: function() {
-			return new Date().getTime() / 1000;
-		},
 		time: function() {
-			return parseInt(this.microtime(),10);
+			return parseInt(microtime(),10);
 		},
 		keyOfHighestValue: function(h) {
 			var count= 0, value=null;
