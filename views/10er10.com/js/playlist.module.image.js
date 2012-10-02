bf52b2cac27a2063c62fd6e891006f4604d4a20c
Mickael Bailly <mickael.bailly@free.fr>
2012-10-02 18:58:55 +0200

    playlist image bigger when available

--- views/10er10.com/js/playlist.module.image.js
+++ views/10er10.com/js/playlist.module.image.js
@@ -1,13 +1,46 @@
 define(["js/domReady","js/d10.playlistModule", "js/playlist", "js/d10.imageUtils"], function(foo, playlistModule, playlist, imageUtils) {
+  
+    var getImageUrl = function(images) {
+      if ( !images || !images.length ) {
+        return false;
+      }
+      var image = images.split(",").shift();
+      return imageUtils.getImageUrl(image);
+    };
+    
+    var getAlternativeImageUrl = function(s) {
+      var alternatives = s.data("image-alternatives");
+      if ( !alternatives ) {
+        return false;
+      }
+      for (var i in alternatives ) {
+        if ( alternatives[i]["250"] ) {
+          return imageUtils.getImageUrl(
+            imageUtils.getAlternateFileName(i,alternatives[i]["250"])
+          );
+        }
+      }
+      for (var i in alternatives ) {
+        if ( alternatives[i]["200"] ) {
+          return imageUtils.getImageUrl(
+            imageUtils.getAlternateFileName(i,alternatives[i]["200"])
+          );
+        }
+      }
+    };
+  
 	var module = new playlistModule("image",{
 		"playlist:currentSongChanged": function() {
 			var s = playlist.current();
 			var images = s.attr("data-images");
-			if ( images && images.length ) {
+            var imageUrl = getAlternativeImageUrl(s);
+            if ( !imageUrl ) {
+              imageUrl = getImageUrl(images);
+            }
+			if ( imageUrl ) {
 				var alreadyVisible = $("#side > .audioImage").find("img").length;
-				var image = images.split(",").shift();
 				$("#side > .audioImage").html(
-					"<img src=\""+imageUtils.getImageUrl(image)+"\">"
+					"<img src=\""+imageUrl+"\">"
 				);
 				if ( ! alreadyVisible ) {
 					$("#side > .audioImage").slideDown("fast");
