79339ea5ea8b3078cf2b2b689db464c4eea077cc
Mickael Bailly <mickael.bailly@free.fr>
2012-10-02 18:12:11 +0200

    Added utility to get alternative image url

--- views/10er10.com/js/d10.imageUtils.js
+++ views/10er10.com/js/d10.imageUtils.js
@@ -1,4 +1,14 @@
 define(["js/config"], function(config) {
+  var splitFileAndExtension = function(filename) {
+    var dotIndex = filename.lastIndexOf(".");
+    if ( dotIndex < 0 ) {
+      return false;
+    }
+    return {
+      name: filename.substr(0,dotIndex),
+      extension: filename.substr( (dotIndex+1) )
+    };
+  };
 	return {
 	getAlbumDefaultImage: function() {
 		var randomnumber=Math.floor(Math.random()*config.img_default.length);
@@ -20,6 +30,18 @@ define(["js/config"], function(config) {
 	},
 	getImageUrl: function(name) {
 		return config.img_root+"/"+encodeURIComponent(name);
-	}
+	},
+    getAlternateFileName: function(filename, size) {
+      var targetFileAndExtension = splitFileAndExtension(filename);
+      if ( !targetFileAndExtension ) {
+        return false;
+      }
+      // somefile.250x245.jpg
+      var targetfileName = targetFileAndExtension.name+
+                          "."+
+                          size.width+"x"+size.height+
+                          "."+targetFileAndExtension.extension;
+      return targetfileName;
+    }
 	};
 });
