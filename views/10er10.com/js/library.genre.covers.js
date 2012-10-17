7617a83ccfd13ca78d30d2673ce535cd875eaf0c
Mickael Bailly <mickael.bailly@free.fr>
2012-10-17 12:04:44 +0200

    album covers popup in genre albums

--- views/10er10.com/js/library.genre.covers.js
+++ views/10er10.com/js/library.genre.covers.js
@@ -17,12 +17,53 @@ define([
         genre: category
       };
       categorydiv.html(tpl.mustacheView("library.content.genre.covers", template_data));
+      var currentOpenedPopin = null;
+      categorydiv.delegate(".albumMini img","mouseenter",function() {
+            var container = $(this).closest(".albumMini");
+            $(this).data("popupTimeout", setTimeout(function() {
+                var widget = $( tpl.mustacheView("library.content.album.all.popover", container.data("albumDetails") ) )
+                .css({
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    visibility: "hidden"
+                }).delegate("a","click",function() {
+                    $(this).closest(".popover").remove();
+                });
+                ;
+                if ( currentOpenedPopin ) {
+                  currentOpenedPopin.remove();
+                }
+                currentOpenedPopin = widget;
+                $("body").append(widget);
+                var srcpos = container.offset(),
+                srcsize = { width: container.outerWidth(), height: container.outerHeight() },
+                widgetsize = { width: widget.outerWidth(), height: widget.outerHeight() },
+                widgetOuter = Math.round( (widgetsize.height - widget.height()) / 2),
+                leftoffset = Math.round((widgetsize.width - srcsize.width) / 2),
+                left = srcpos.left - leftoffset;
+                if ( left < 0 ) { left = 0 ; }
+                widget.css({
+                    top: srcpos.top - widgetOuter,
+                    left: left,
+                    visibility: "visible"
+                }).mouseleave(function() {$(this).remove();}).addClass("on");
+            },10));
+        })
+        .delegate(".albumMini img","mouseleave",function() {
+            var tid = $(this).data("popupTimeout");
+            if(tid) {
+                clearTimeout(tid);
+            }
+        })
       //rest.genre.albumsSongs(genre, query, options)
       var restWrapper = function(query, options) {
         return rest.genre.albumsSongs(category, query, options);
       };
       var cursor = new restHelpers.couchMapMergedCursor(restWrapper, {}, "album");
       var section = categorydiv.find("section");
+      var loadTimeout = null;
+      var innerLoading = categorydiv.find(".innerLoading");
       var scrollOpts = {
         pxMin: 150,
         fetchToFill: true,
@@ -38,6 +79,39 @@ define([
             }
           });
           return albums;
+        },
+        onFirstContent: function() {
+          var list = categorydiv.find(".list");
+          var grippie = section.next(".grippie");
+          grippie.show();
+          section.makeResizable(
+            {
+              vertical: true,
+              minHeight: 100,
+              maxHeight: function() {
+                  // always the scrollHeight
+                  var sh = list.prop("scrollHeight");
+                  if ( sh ) {
+                      return sh -10;
+                  }
+                  return 0;
+              },
+              grippie: grippie
+            }
+          );
+        },
+       onQuery: function() {
+          loadTimeout = setTimeout(function() {
+            loadTimeout = null;
+            innerLoading.css("top", section.height() - 32).removeClass("hidden");
+          },500);
+        },
+        onContent: function() {
+          if ( loadTimeout ) {
+            clearTimeout(loadTimeout);
+          } else {
+            innerLoading.addClass("hidden");
+          }
         }
       };
       section.data("infiniteScroll",
