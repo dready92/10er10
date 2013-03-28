ab2a7e845fd3da3788367578520d5dbd949f0a28
dready92 <mickael.bailly@free.fr>
2013-03-28 22:36:52 +0100

    refactor websocket router

--- node/lib/websocket-server.js
+++ node/lib/websocket-server.js
@@ -2,22 +2,38 @@ var debug = require("debug")("d10:websocket-server");
 
 function websocketServer ( httpServer, d10Server ) {
   var wsId = 1;
+  var sockets = {};
   var wsServer = require('ws').Server;
   var d10wsServer = new wsServer({server: httpServer});
-  var wsRouter = require("./websocket-router");
-  var d10wsrouter = new wsRouter();
-  var wrestProtocol = require("./websocket-protocol-wrest");
-  var wrestProtocolInstance = new wrestProtocol(httpServer, d10Server);
-  d10wsrouter.addType(wrestProtocolInstance.name, wrestProtocolInstance.handler);
+  this.createRouter();
+  this.registerProtocols();
   d10wsServer.on('connection', function(ws) {
     ws.d10id = wsId++;
+    sockets[ws.d10id] = ws;
     ws.on('close', function() {
+      delete sockets[ws.d10id];
       debug("socket close");
     });
     ws.on('message', function(message) {
-      d10wsrouter.route(message,ws);
-    });
+      this.router.route(message,ws);
+    }.bind(this));
   });
+};
+
+websocketServer.prototype.router = null;
+websocketServer.prototype.createRouter = function() {
+  var wsRouter = require("./websocket-router");
+  this.router = new wsRouter();
+};
+
+websocketServer.prototype.registerProtocols = function() {
+  this.registerWrestProtocol();
+};
+
+websocketServer.prototype.registerWrestProtocol = function() {
+  var wrestProtocol = require("./websocket-protocol-wrest");
+  var wrestProtocolInstance = new wrestProtocol(httpServer, d10Server);
+  this.router.addType(wrestProtocolInstance.name, wrestProtocolInstance.handler);
 };
 
 exports = module.exports = websocketServer;
