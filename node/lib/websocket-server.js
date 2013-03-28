5b596fb34a8bf27c355610067ab29b30dda3bbd3
dready92 <mickael.bailly@free.fr>
2013-03-28 22:40:35 +0100

    bugfix on websocket server refactoring

--- node/lib/websocket-server.js
+++ node/lib/websocket-server.js
@@ -6,7 +6,7 @@ function websocketServer ( httpServer, d10Server ) {
   var wsServer = require('ws').Server;
   var d10wsServer = new wsServer({server: httpServer});
   this.createRouter();
-  this.registerProtocols();
+  this.registerProtocols(httpServer, d10Server);
   d10wsServer.on('connection', function(ws) {
     ws.d10id = wsId++;
     sockets[ws.d10id] = ws;
@@ -26,11 +26,11 @@ websocketServer.prototype.createRouter = function() {
   this.router = new wsRouter();
 };
 
-websocketServer.prototype.registerProtocols = function() {
-  this.registerWrestProtocol();
+websocketServer.prototype.registerProtocols = function(httpServer, d10Server) {
+  this.registerWrestProtocol(httpServer, d10Server);
 };
 
-websocketServer.prototype.registerWrestProtocol = function() {
+websocketServer.prototype.registerWrestProtocol = function(httpServer, d10Server) {
   var wrestProtocol = require("./websocket-protocol-wrest");
   var wrestProtocolInstance = new wrestProtocol(httpServer, d10Server);
   this.router.addType(wrestProtocolInstance.name, wrestProtocolInstance.handler);
