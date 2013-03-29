d0b3692ef6f884d64476eca43a108094c7ba13e5
Mickael Bailly <mickael.bailly@free.fr>
2013-03-29 18:39:54 +0100

    use ogg length external file task

--- node/lib/song-processor.js
+++ node/lib/song-processor.js
@@ -140,17 +140,7 @@ function processSong(songId, songFilename, songFilesize, userId, readableStream,
           },
           oggLength: {
               status: null,
-              run: function(then) {
-                  var file = (this.tasks.fileType.response == "application/ogg" ) ? this.fileName : this.oggName ;
-                  audioUtils.oggLength(d10.config.audio.tmpdir+"/"+file,function(err,len) {
-                      if ( !err ) {
-                          if ( len && len.length && len.length > 2 ) {
-                              len = 60*parseInt(len[1],10) + parseInt(len[2],10);
-                          }
-                      }
-                      then(err,len);
-                  });
-              }
+              run: require("./song-processor/task-ogg-length")
           },
           sha1File: {
               status: null,
