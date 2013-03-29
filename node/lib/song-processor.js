52054fb28a2999718e634e7751dfe10d3ab82cc2
Mickael Bailly <mickael.bailly@free.fr>
2013-03-29 19:14:44 +0100

    extract cleanup tags task to external file

--- node/lib/song-processor.js
+++ node/lib/song-processor.js
@@ -102,36 +102,7 @@ function processSong(songId, songFilename, songFilesize, userId, readableStream,
           },
           cleanupTags: {
               status: null,
-              run: function(then) {
-  //                      if ( this.tasks.fileMeta.err ) {
-  //                          cleanupFileSystem();
-  //                          return ;
-  //                      }
-                  if ( !this.tasks.fileMeta.response ) { this.tasks.fileMeta.response={}; }
-                  var tags = {};
-                  var that=this;
-                  
-                  if ( this.tasks.fileMeta.response.GENRE ) {
-                      var value = "";
-                      d10.config.genres.forEach(function(v,k) {
-                          if ( that.tasks.fileMeta.response.GENRE == v.toLowerCase() ) {
-                              value=v;
-                          }
-                      });
-                      tags.genre = value.length ? value : that.tasks.fileMeta.response.GENRE ;
-                  }
-                  
-                  ['ALBUM','ARTIST','TITLE'].forEach(function(v,k) {
-                      if ( that.tasks.fileMeta.response[v] ) {
-                          tags[v] = d10.ucwords(that.tasks.fileMeta.response[v].toLowerCase());
-                      }
-                  });
-                  ['ALBUM','TRACKNUMBER','ARTIST','TITLE','GENRE','DATE'].forEach(function(v,k) {
-                      if ( that.tasks.fileMeta.response[v] ) { tags[v] = that.tasks.fileMeta.response[v]; }
-                      else { tags[v] = null }
-                  });
-                  then(null,tags);
-              }
+              run: require("./song-processor/task-cleanup-tags")
           },
           moveAlternativeFile: {
             status: null,
