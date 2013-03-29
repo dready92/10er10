bbcb650e491f69c303bdc2b572470070e3da0ffa
Mickael Bailly <mickael.bailly@free.fr>
2013-03-29 19:45:01 +0100

    extract createDocument task to an external file

--- node/lib/song-processor.js
+++ node/lib/song-processor.js
@@ -63,6 +63,7 @@ function processSong(songId, songFilename, songFilesize, userId, readableStream,
   var job = {
       readableStream: readableStream,
       id: songId,
+      userId: userId,
       songFilename: songFilename,
       fileName: songId+".mp3",
       fileSha1: null,
@@ -118,90 +119,7 @@ function processSong(songId, songFilename, songFilesize, userId, readableStream,
           },
           createDocument: {
               status: null,
-              run: function(then) {
-                  
-                  var resp = this.tasks.oggLength.response, duration = 0;
-                  var sha1 = this.tasks.sha1File.response;
-                  if ( 
-                      Object.prototype.toString.call(resp) === '[object Array]' &&
-                      resp.length > 2 && 
-                      !isNaN(parseFloat(resp[1])) && 
-                      !isNaN(parseFloat(resp[2])) 
-                  ) {
-                      duration = parseFloat(resp[1])*60 + parseFloat(resp[2]);
-                  } else {
-                      duration = this.tasks.oggLength.response;
-                  }
-                  
-                  var doc = {
-                      _id: this.id,
-                      filename: this.songFilename,
-                      sha1: sha1,
-                      user: userId,
-                      reviewed: false,
-                      valid: false,
-                      ts_creation: new Date().getTime(),
-                      hits: 0,
-                      duration: duration
-                  };
-                  if ( this.tasks.moveAlternativeFile.response ) {
-                    doc.sourceFile = this.tasks.moveAlternativeFile.response;
-                  }
-                  for ( var index in this.tasks.cleanupTags.response ) {
-                      var k = index.toLowerCase(),
-                          v = this.tasks.cleanupTags.response[index];
-                      if ( k == "date" || k == "tracknumber" ) {
-                          doc[k] = parseFloat(v);
-                          if ( isNaN(doc[k]) )    doc[k]=0;
-                      } else {
-                          doc[k] = v;
-                      }
-                  }
-                  if ( typeof doc.title == "string" && doc.title.length &&
-                      typeof doc.artist == "string" && doc.artist.length ) {
-                      doc.valid = true;
-                  }
-                  
-                  // test for tracknumber and get it from filename if possible
-                  if ( doc.tracknumber == 0 ) {
-                      var tracknumberFromFilename = doc.filename.match(/^[0-9]+/);
-                      if ( tracknumberFromFilename ) {
-                          doc.tracknumber = parseInt( tracknumberFromFilename[0] , 10);
-                      }
-                  }
-                  
-  //                          return then(null,doc);
-                  var recordDoc = function() {
-                    d10.couch.d10.view("song/sha1",{key: sha1}, function(err,resp) {
-                        if ( err ) {
-                            then(501);
-                        } else if (!resp.rows || resp.rows.length) {
-                            then(433);
-                        } else {
-                          d10.couch.d10.storeDoc(doc,function(err,resp) {
-                            if ( err ) { then(err); }
-                            else {
-                              doc._rev = resp.rev;
-                              then(null,doc);
-                            }
-                          });
-                        }
-                    });
-                      
-                  };
-
-                  if ( this.tasks.fileMeta.response && this.tasks.fileMeta.response.PICTURES && this.tasks.fileMeta.response.PICTURES.length ) {
-                      gu.imageFromMeta(this.tasks.fileMeta.response,function(err,resp) {
-                          debug(songId,"imageFromMeta response",err,resp);
-                          if ( !err ) {
-                              doc.images = [ resp ];
-                          }
-                          recordDoc();
-                      });
-                  } else {
-                      recordDoc();
-                  }
-              }
+              run: require("./song-processor/task-create-document")
           }
       },
       queue: [],
