8dbaec3b70234a2f64394eb362e7d7fb3e1eda7b
Mickael Bailly <mickael.bailly@free.fr>
2013-06-05 19:05:01 +0200

    add helper to set cookie

--- node/contextMiddleware.js
+++ node/contextMiddleware.js
@@ -1,6 +1,7 @@
 var 	path = require("path"),
 		url = require("url"),
-		qs = require("qs");
+		qs = require("qs"),
+		d10 = require("./d10");
 
 
 exports.context = function (req,res,next) { 
@@ -10,7 +11,16 @@ exports.context = function (req,res,next) {
 		headers: {Connection: "close"},
 		status: 404,
 		session: {},
-        remoteControlSession: {}
+        remoteControlSession: {},
+        setCookie: function(cookie) {
+          var d = new Date();
+          d.setTime ( d.getTime() + d10.config.cookieTtl );
+          this.headers["Set-Cookie"] = d10.config.cookieName+
+                                              "="+
+                                              escape(JSON.stringify(cookie))+
+                                              "; expires="+d.toUTCString()+
+                                              "; path="+d10.config.cookiePath;
+        }
 	};
 	var u = url.parse("http://"+req.headers.host+req.url);
 	req.query = u.query ? qs.parse(u.query) : {};
