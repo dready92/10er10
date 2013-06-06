85b68ae7af9b3c3a3b98ba7e39134f8e93f083ed
Mickael Bailly <mickael.bailly@free.fr>
2013-06-06 14:55:33 +0200

    don't keep a reference to webwocket in the websocketserver

--- node/lib/websocket-server.js
+++ node/lib/websocket-server.js
@@ -1,17 +1,17 @@
 var debug = require("debug")("d10:websocket-server");
 var util = require("util");
 var dosWatchdog = require("../dosWatchdog");
+var webSocketStore = require("./websocket-store");
+
 function websocketServer ( httpServer, d10Server ) {
   var wsId = 1;
-  var sockets = {};
   var wsServer = require('ws').Server;
   var d10wsServer = new wsServer({server: httpServer});
   this.createRouter();
   this.registerProtocols(httpServer, d10Server);
   d10wsServer.on('connection', function(ws) {
     ws.d10id = wsId++;
-    sockets[ws.d10id] = ws;
-    this.bindWebSocketListeners(ws, sockets);
+    this.bindWebSocketListeners(ws);
   }.bind(this));
   
   d10wsServer.on('error', function(err) {
@@ -48,11 +48,7 @@ websocketServer.prototype.registerPevtsProtocol = function(httpServer, d10Server
   this.router.addType(pevtsProtocolInstance.name, pevtsProtocolInstance.handler);
 };
 
-websocketServer.prototype.bindWebSocketListeners = function(ws, sockets) {
-  ws.on('close', function() {
-    delete sockets[ws.d10id];
-    debug("socket close");
-  }.bind(this));
+websocketServer.prototype.bindWebSocketListeners = function(ws) {
   ws.on('message', function(message) {
     this.router.route(message,ws);
   }.bind(this));
