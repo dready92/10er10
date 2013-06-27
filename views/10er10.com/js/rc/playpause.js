1f35b047810aa81156cf4d43b31cdeffe4bc3b97
dready92 <mickael.bailly@free.fr>
2013-06-27 22:12:21 +0200

    implement next button

--- views/10er10.com/js/rc/playpause.js
+++ views/10er10.com/js/rc/playpause.js
@@ -8,23 +8,38 @@
       templateUrl: '../html/rc/controls/playpause.html',
       replace: true,
       link: function ($scope) {
-        $scope.d10playpause = {
-          play: function(evt) {
-            debug("sending play command to peer");
-            remot.play(function(err) {
-              debug("play command response: ",err);
-            });
-            evt.stopPropagation();
-            evt.preventDefault();
-          },
-          pause: function(evt) {
-            debug("sending pause command to peer");
-            remot.pause(function(err) {
-              debug("pause command response: ",err);
-            });
-            evt.stopPropagation();
-            evt.preventDefault();
-          }
+        $scope.d10playpause = $scope.d10playpause || {};
+        $scope.d10playpause.play = function(evt) {
+          debug("sending play command to peer");
+          remot.play(function(err) {
+            debug("play command response: ",err);
+          });
+          evt.stopPropagation();
+          evt.preventDefault();
+        };
+        $scope.d10playpause.pause = function(evt) {
+          debug("sending pause command to peer");
+          remot.pause(function(err) {
+            debug("pause command response: ",err);
+          });
+          evt.stopPropagation();
+          evt.preventDefault();
+        };
+      }
+    };
+  })
+  .directive("d10playnext",function() {
+    var remot = require("js/d10.websocket.protocol.remot");
+    return {
+      restrict: 'A',
+      templateUrl: '../html/rc/controls/playnext.html',
+      replace: true,
+      link: function ($scope) {
+        $scope.d10playpause = $scope.d10playpause || {};
+        $scope.d10playpause.playNext = function() {
+          remot.next(function(err,done) {
+            debug("next command response: ",err,"done:",done);
+          });
         };
       }
     };
