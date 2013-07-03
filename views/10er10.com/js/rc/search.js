fd31c38284e704c9190972a85e988a07d26dd5a4
Mickael Bailly <mickael.bailly@free.fr>
2013-07-03 17:01:03 +0200

    work on the search feature

--- views/10er10.com/js/rc/search.js
+++ views/10er10.com/js/rc/search.js
@@ -4,10 +4,59 @@
   .controller("d10searchController", [
   "$scope",
   "$location",
-  function($scope, $location) {
+  "d10search",
+  function($scope, $location,d10search) {
+    $scope.searchInProgress = false;
+    $scope.query = "";
+    $scope.results = {
+      title: [],
+      artist: [],
+      album: []
+    };
+    $scope.resultsCount = 0;
+    
+    $scope.getResultsCount = function() {
+      $scope.resultsCount = 0;
+      if ( $scope.results && $scope.results.title ) {
+        $scope.resultsCount += $scope.results.title;
+      }
+      if ( $scope.results && $scope.results.artist ) {
+        $scope.resultsCount += $scope.results.artist;
+      }
+      if ( $scope.results && $scope.results.album ) {
+        $scope.resultsCount += $scope.results.album;
+      }
+      return $scope.resultsCount;
+    };
+    
     $scope.home = function() {
       $location.path("/main");
     };
-  }]);
+    
+    $scope.search = function() {
+      if ( !$scope.query || $scope.query.length < 3 ) {
+        return false;
+      }
+      $scope.searchInProgress = true;
+      d10search($scope.query, function(err,resp) {
+        $scope.$apply(function() {
+          $scope.searchInProgress = false;
+          if ( !err ) {
+            $scope.results.title = resp.title;
+            $scope.results.album = resp.album;
+            $scope.results.artist = resp.artist;
+          }
+          $scope.getResultsCount();
+        });
+      });
+    };
+    
+  }])
+  .factory("d10search", function() {
+    var rest = require("js/d10.rest");
+    return function(query, callback) {
+      rest.search.all(query, {load: callback});
+    };
+  });
   
 })();
