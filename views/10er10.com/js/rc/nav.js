c98dabe2878a35131a47e340bbabf5ec6ad98a90
Mickael Bailly <mickael.bailly@free.fr>
2013-07-03 17:00:51 +0200

    add a tab/pane helper

--- views/10er10.com/js/rc/nav.js
+++ views/10er10.com/js/rc/nav.js
@@ -7,6 +7,22 @@
       replace: false,
       templateUrl: '../html/rc/nav/inside.html'
     };
-  });
+  })
+  .directive("d10tab", function() {
+    return {
+      restrict: 'A',
+      replace: false,
+      link: function($scope,$element,$attrs) {
+        $scope.currentPane = null;
+        $scope.setCurrentPane = function(name) {
+          $scope.currentPane = name;
+        };
+        $scope.getCurrentPane = function() {
+          return $scope.currentPane;
+        };
+      }
+    };
+  })
+  ;
   
 })();
