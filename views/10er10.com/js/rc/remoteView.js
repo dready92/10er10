642d30ae4701de26204bb9df17dece369d756f46
Mickael Bailly <mickael.bailly@free.fr>
2013-10-05 16:35:44 +0200

    [improvement] remember scrolling position of remoteView

--- views/10er10.com/js/rc/remoteView.js
+++ views/10er10.com/js/rc/remoteView.js
@@ -18,15 +18,38 @@ angular.module("d10remoteView").controller("d10remoteViewController",
                                            "$location",
                                            "d10rc",
                                            'd10rcView',
+                                           '$window',
+                                           '$timeout',
+                                           'd10remoteViewPageCache',
                                            function(
                                              $scope, 
                                              $location,
                                              d10remoteControl,
-                                             d10rcView
+                                             d10rcView,
+                                             $window,
+                                             $timeout,
+                                             d10remoteViewPageCache
                                                    ) {
+  d10remoteViewPageCache.scrollY = d10remoteViewPageCache.scrollY || 0;
+  var scrollY = d10remoteViewPageCache.scrollY;
+  function onWindowScroll() {
+    scrollY = $window.scrollY;
+  };
   $scope.remoteView = d10rcView;
   $scope.remoteControl = d10remoteControl;
-}]);
+  angular.element($window).bind("scroll",onWindowScroll);
+  $scope.$on("$destroy",function() {
+    angular.element($window).unbind("scroll",onWindowScroll);
+    d10remoteViewPageCache.scrollY = scrollY;
+  });
+  
+  $timeout(function() {
+    $window.scrollTo(0,scrollY);
+  },0);
+}])
+.factory("d10remoteViewPageCache",function() {
+  return {};
+});
 
 
 })();
