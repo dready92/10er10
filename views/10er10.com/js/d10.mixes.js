caf8c637e048ff923fda88455d6b12c442edfce1
Michael Bailly <mbailly@linagora.com>
2017-05-23 12:09:13 +0200

    Add new mixes

--- views/10er10.com/js/d10.mixes.js
+++ views/10er10.com/js/d10.mixes.js
@@ -1,6 +1,6 @@
 define(["js/d10.mix"],function(d10mix) {
   var mixes = [];
-      mixes.push (
+  mixes.push (
     {
       label: "Very fast fade",
       description: "Very fast fade out/in (3 secs)",
@@ -33,7 +33,7 @@ define(["js/d10.mix"],function(d10mix) {
       }
     }
   );
-  
+
   mixes.push (
     {
       label: "Fast fade",
@@ -67,7 +67,7 @@ define(["js/d10.mix"],function(d10mix) {
       }
     }
   );
-  
+
   mixes.push (
     {
       label: "Medium fade",
@@ -101,7 +101,7 @@ define(["js/d10.mix"],function(d10mix) {
       }
     }
   );
-  
+
   mixes.push (
     {
       label: "Long fade",
@@ -135,7 +135,7 @@ define(["js/d10.mix"],function(d10mix) {
       }
     }
   );
-  
+
   mixes.push (
     {
       label: "explosion",
@@ -244,6 +244,210 @@ define(["js/d10.mix"],function(d10mix) {
     }
   );
 
+  mixes.push (
+    {
+      label: "gunShot",
+      description: "gun loaded, then three bullets fired",
+      builder: function() {
+        var steps = [];
+        steps.push(
+          new d10mix.mixStep(
+            "guncock",
+            0,
+            0,
+            "volume",
+            d10mix.mixStep.PROPERTY_VALUE_CURRENT_VOLUME,
+            {
+              propertyStartValue: d10mix.mixStep.PROPERTY_VALUE_CURRENT_VOLUME,
+              startPlaybackOnBegin: true
+            })
+        );
+        steps.push(
+          new d10mix.mixStep(
+            "shot1",
+            2,
+            0,
+            "volume",
+            d10mix.mixStep.PROPERTY_VALUE_CURRENT_VOLUME,
+            {
+              propertyStartValue: d10mix.mixStep.PROPERTY_VALUE_CURRENT_VOLUME,
+              startPlaybackOnBegin: true
+            })
+        );
+        steps.push(
+          new d10mix.mixStep(
+            "shot2",
+            2.6,
+            0,
+            "volume",
+            d10mix.mixStep.PROPERTY_VALUE_CURRENT_VOLUME,
+            {
+              propertyStartValue: d10mix.mixStep.PROPERTY_VALUE_CURRENT_VOLUME,
+              startPlaybackOnBegin: true
+            })
+        );
+        steps.push(
+          new d10mix.mixStep(
+            "shot3",
+            3.5,
+            0,
+            "volume",
+            d10mix.mixStep.PROPERTY_VALUE_CURRENT_VOLUME,
+            {
+              propertyStartValue: d10mix.mixStep.PROPERTY_VALUE_CURRENT_VOLUME,
+              startPlaybackOnBegin: true
+            })
+        );
+        steps.push(
+          new d10mix.mixStep(
+            "currentTrack",
+            2,
+            1,
+            "volume",
+            0,
+            {
+              propertyStartValue: d10mix.mixStep.PROPERTY_VALUE_CURRENT_VOLUME,
+              stopPlaybackOnEnd: true
+            }
+          )
+        );
+        steps.push(
+          new d10mix.mixStep(
+            "nextTrack",
+            3,
+            0,
+            "volume",
+            d10mix.mixStep.PROPERTY_VALUE_CURRENT_VOLUME,
+            {
+              propertyStartValue: d10mix.mixStep.PROPERTY_VALUE_CURRENT_VOLUME,
+              startPlaybackOnBegin: true
+            }
+          )
+        );
+        var assets = [
+          {label: "guncock",url: "/css/mix/90965-gun-cocking.ogg"},
+          {label: "shot1",url: "/css/mix/gun-shot.ogg"},
+          {label: "shot2",url: "/css/mix/gun-shot.ogg"},
+          {label: "shot3",url: "/css/mix/gun-shot.ogg"}
+        ];
+        return new d10mix.mix(assets, steps);
+      }
+    }
+  );
+
+  mixes.push (
+    {
+      label: "switchingAlarm",
+      description: "A kind of strange alarm rings",
+      builder: function() {
+        var steps = [];
+        steps.push(
+          new d10mix.mixStep(
+            "alarm",
+            0,
+            1.5,
+            "volume",
+            d10mix.mixStep.PROPERTY_VALUE_CURRENT_VOLUME,
+            {
+              propertyStartValue: 0,
+              startPlaybackOnBegin: true
+            })
+        );
+        steps.push(
+          new d10mix.mixStep(
+            "currentTrack",
+            0,
+            3,
+            "volume",
+            0,
+            {
+              propertyStartValue: d10mix.mixStep.PROPERTY_VALUE_CURRENT_VOLUME,
+              stopPlaybackOnEnd: true
+            }
+          )
+        );
+        steps.push(
+          new d10mix.mixStep(
+            "nextTrack",
+            4,
+            1,
+            "volume",
+            d10mix.mixStep.PROPERTY_VALUE_CURRENT_VOLUME,
+            {
+              propertyStartValue: 0,
+              startPlaybackOnBegin: true
+            }
+          )
+        );
+        var assets = [
+          {label: "alarm",url: "/css/mix/340950-alarm.ogg"}
+        ];
+        return new d10mix.mix(assets, steps);
+      }
+    }
+  );
+
+  mixes.push (
+    {
+      label: "increaseRate",
+      description: "Increase next song rate",
+      builder: function() {
+        var steps = [];
+        steps.push(
+          new d10mix.mixStep(
+            "currentTrack",
+            1,
+            5,
+            "volume",
+            0,
+            {stopPlaybackOnEnd: true}
+          )
+        );
+        steps.push(
+          new d10mix.mixStep(
+            "currentTrack",
+            0,
+            4,
+            "playbackRate",
+            0.7,
+            {
+              propertyStartValue: 1,
+              stopPlaybackOnEnd: true
+            }
+          )
+        );
+        steps.push(
+          new d10mix.mixStep(
+            "nextTrack",
+            2,
+            3,
+            "volume",
+            d10mix.mixStep.PROPERTY_VALUE_CURRENT_VOLUME,
+            {
+              propertyStartValue: 0,
+              startPlaybackOnBegin: true
+            }
+          )
+        );
+        steps.push(
+          new d10mix.mixStep(
+            "nextTrack",
+            2,
+            5,
+            "playbackRate",
+            1,
+            {
+              propertyStartValue: 0.7,
+              startPlaybackOnBegin: true
+            }
+          )
+        );
+        return new d10mix.mix([], steps);
+      }
+    }
+  );
+
+
   return mixes;
-  
+
 });
