9939c84f223fbdc1d6f3f2be1bfb45ac3d3878bb
Michael Bailly <mbailly@linagora.com>
2018-04-10 23:15:59 +0200

    Fix import-db.js admin script

--- node/admin/import-db.js
+++ node/admin/import-db.js
@@ -1,149 +1,169 @@
 var prompt = require("prompt"),
-		fs = require("fs"),
-	config = require("../config");
+  fs = require("fs"),
+  config = require("../config");
 
-var target, dirname, d10;
+let target;
+let dirname;
+let d10;
 
-
-
-var getTarget = function() {
-	prompt().ask("Which db to import to ? production (p) or test (t)","target")
-	.tap(
-		function(vars) {
-			target = vars.target.toString().replace(/\s+$/,"");
-			//console.log(target);
-			if ( target != "p" && target != "t" ) {
-				console.log("Bad answer... need p or t");
-				process.exit(1);
-			}
-			if ( target == "p" ) {
-                		config.couch = config.couch_prod;
-		        } else {
-                		config.couch = config.couch_dev;
-		        }
-		        d10 = require("../d10");
-		        console.log("using databases ",config.couch);
-
-		}
-	).ask("Please give the directory to read files from :","dirname")
-        .tap(
-                function(vars) {
-                        dirname = vars.dirname.toString().replace(/\s+$/,"");
-                        if ( dirname.length == 0 ) {
-                                dirname = ".";
-                        }
-                        checkDirname();
-                }
-        ).end();
+const schema = {
+  properties: {
+    target: {
+      description: 'Which db to import to ? production (p) or test (t)',
+      type: 'string',
+      required: true,
+    },
+    dirname: {
+      description: 'Please give the directory to read files from',
+      type: 'string',
+      required: true,
+    }
+  }
 };
 
-var checkDirname = function() {
-	fs.stat(dirname,function(err,stat) {
-		if ( err ) {
-			console.log("Directory error : ", err.message);
-			process.exit(1);
-		}
-		if ( !stat.isDirectory() ) {
-			console.log("Directory error : ",dirname," is not a valid directory");
-			process.exit(1);
-		}
+prompt.start();
 
-		var dbs = [];
-		for ( var i in config.couch ) {
-			dbs.push(i);
-	        }
+prompt.get(schema, (err, result) => {
+  if (err) {
+    console.log('Error', err);
+    process.exit(1);
+  }
 
-		var checkFile = function() {
-			if ( !dbs.length ) {
-				return setupConfig();
-			}
-			var db = dbs.pop();
-			fs.stat(dirname+"/"+db+"-design.json",function(err,stat) {
-				if ( err ) {
-					console.log("Error: file "+dirname+"/"+db+"-design.json");
-					process.exit(1);
-				}else {
-					console.log("File "+dirname+"/"+db+"-design.json found");
-				}
-				checkFile();
-			});
-		};
-		checkFile();
+  target = onTarget(result.target);
+  dirname = onDirname(result.dirname);
 
-	});
+  setupTargetConfig(target);
+  checkDirname();
+});
+
+function onTarget(targetEntry) {
+  const t = targetEntry.toString().replace(/\s+$/, '');
+  if (t !== 'p' && t !== 't') {
+    console.log("Bad answer... need p or t");
+    process.exit(1);
+  }
+
+  return t;
+}
+
+function setupTargetConfig(t) {
+  if (t === 'p') {
+    config.couch = config.couch_prod;
+  } else {
+    config.couch = config.couch_dev;
+  }
+  d10 = require('../d10');
+  d10.setConfig(config);
+  console.log('using databases ', config.couch);
+}
+
+function onDirname(dirnameEntry) {
+  let dn = dirnameEntry.toString().replace(/\s+$/, "");
+  if (dn.length === 0) {
+    dn = '.';
+  }
+
+  return dn;
+}
+
+function checkDirname() {
+  fs.stat(dirname, (err, stat) => {
+    if (err) {
+      console.log("Directory error : ", err.message);
+      process.exit(1);
+    }
+    if (!stat.isDirectory()) {
+      console.log("Directory error : ", dirname, " is not a valid directory");
+      process.exit(1);
+    }
+
+    const dbs = [];
+    for (let i in config.couch) {
+      dbs.push(i);
+    }
+
+    function checkFile() {
+      if (!dbs.length) {
+        return setupConfig();
+      }
+      const db = dbs.pop();
+      fs.stat(dirname + "/" + db + "-design.json", (err, stat) => {
+        if (err) {
+          console.log("Error: file "+dirname+"/"+db+"-design.json", err);
+          process.exit(1);
+        } else {
+          console.log("File " + dirname + "/" + db + "-design.json found");
+        }
+        checkFile();
+      });
+    }
+
+    checkFile();
+  });
 };
 
-var setupConfig = function() {
+function setupConfig() {
+  const todo = [];
+  function then(err) {
+    if (err) {
+      console.log("Failed to import DB: ",err);
+      process.exit(1);
+    }
+    if (todo.length) {
+      todo.pop()();
+    } else {
+      console.log("End of import");
+      process.exit(0);
+    }
+  }
+
+  for (var i in config.couch) {
+    todo.push(
+      (function(cfg) {
+        return function() { importDb(cfg, then);};
+      })(i)
+    );
+  }
+
+  then();
+}
 
 
-	var todo = [];
-	var then = function(err) {
-		if ( err ) {
-			console.log("Failed to import DB: ",err);
-			process.exit(1);
-		}
-		if ( todo.length ) {
-			todo.pop()();
-		} else {
-			console.log("End of import");
-			process.exit(0);
-		}
-	};
+function importDb(db, then) {
+  console.log("importing ", db);
+  const filename = dirname + "/" + db + "-design.json";
+  fs.readFile(filename, (err, str) => {
+    if ( err ) {
+      console.log("Can't read "+filename);
+      then(err);
+    } else {
+      let design = null;
+      try {
+        design = JSON.parse(str);
+      } catch (e) { design = null; }
+      if ( !design ) {
+        then ({message: "JSON parsing failed"});
+      } else {
+        function insertOne() {
+          if ( !design.length ) {
+            return then();
+          }
+          var doc = design.pop();
+          console.log('Inserting ' + doc._id);
 
-
-	for ( var i in config.couch ) {
-		todo.push(
-			(function(cfg) {
-				return function() { importDb(cfg,then);};
-			})(i)
-		);
-	}
-
-	then();
+          d10.couch[db].deleteDoc(doc._id, function (err, resp, m) {
+            if (err)	console.log(err,resp,m);
+            d10.couch[db].storeDoc(doc,function(err,resp) {
+              if ( err ) {
+                then(err);
+              } else {
+                insertOne();
+              }
+            });
+          });
+        };
+        insertOne();
+      }
+    }
+  });
 };
-
-
-var importDb = function(db,then) {
-	console.log("importing ",db);
-	var filename = dirname+"/"+db+"-design.json";
-	fs.readFile(filename,function(err,str) {
-		if ( err ) {
-			console.log("Can't read "+filename);
-			then(err);
-		} else {
-			var design = null;
-			try {
-				design = JSON.parse(str);
-			} catch (e) { design = null }
-			if ( !design ) {
-				then ( {message: "JSON parsing failed"} );
-			} else {
-				var insertOne = function() {
-					if ( !design.length ) {
-						return then();
-					}
-					var doc = design.pop();
-					console.log("Inserting "+doc._id);
-					d10.couch[db].deleteDoc(doc._id,function(err,resp,m) {
-						if ( err )	console.log(err,resp,m);
-						d10.couch[db].storeDoc(doc,function(err,resp) {
-							if ( err ) {
-								then(err);
-							} else {
-								insertOne();
-							}
-						});
-					});
-				};
-				insertOne();
-			}
-		}
-	});
-};
-
-
-
-
-getTarget();
-
-
