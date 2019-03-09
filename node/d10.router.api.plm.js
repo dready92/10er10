cbb5135beac3a20aee011906135245b117ccef46
Michael Bailly <mbailly@linagora.com>
2019-03-09 20:07:38 +0100

    Lint

--- node/d10.router.api.plm.js
+++ node/d10.router.api.plm.js
@@ -1,120 +1,116 @@
-var d10 = require ("./d10"),
-	querystring = require("querystring"),
-	qs = require("qs"),
-	rpl = require("./d10.rpl"),
-	bodyParser = require('body-parser'),
-	jsonParserMiddleware = bodyParser.json();
+const bodyParser = require('body-parser');
+const rpl = require('./d10.rpl');
+const d10 = require('./d10');
 
-exports.api = function(app) {
+const jsonParserMiddleware = bodyParser.json();
 
-	app.get("/api/plm/pl*",function(request,response) {
-		rpl.playlistAndSongs("pl"+request.params[0],function(err,playlist) {
-			if ( err ) {
-				return d10.realrest.err(423,null,request.ctx);
-			}
-			if ( playlist.user != request.ctx.user._id ) {
-				return d10.realrest.err(403,null,request.ctx);
-			}
-			d10.realrest.success(playlist,request.ctx);
-		});
-	});
+exports.api = function api(app) {
+  app.get('/api/plm/pl*', (request) => {
+    rpl.playlistAndSongs(`pl${request.params[0]}`, (err, playlist) => {
+      if (err) {
+        return d10.realrest.err(423, null, request.ctx);
+      }
+      if (playlist.user !== request.ctx.user._id) {
+        return d10.realrest.err(403, null, request.ctx);
+      }
+      return d10.realrest.success(playlist, request.ctx);
+    });
+  });
 
-	app.put("/api/plm/update", jsonParserMiddleware, function(request,response) {
-		if ( !request.body.playlist ) {
-			return d10.realrest.err(427,"playlist parameter is empty",request.ctx);
-		}
-		var songs = request.body["songs[]"] ? request.body["songs[]"] : [];
-		if ( Object.prototype.toString.call(songs) !== '[object Array]' ) {
-			songs = [ songs ];
-		}
+  app.put('/api/plm/update', jsonParserMiddleware, (request) => {
+    if (!request.body.playlist) {
+      return d10.realrest.err(427, 'playlist parameter is empty', request.ctx);
+    }
+    let songs = request.body['songs[]'] ? request.body['songs[]'] : [];
+    if (Object.prototype.toString.call(songs) !== '[object Array]') {
+      songs = [songs];
+    }
 
-		d10.couch.d10.getDoc(request.body.playlist,function(err,playlist) {
-			if ( err) {
-				return d10.realrest.err(423,err,request.ctx);
-			}
-			if ( playlist.user != request.ctx.user._id ) {
-				return d10.realrest.err(403,null,request.ctx);
-			}
-			rpl.update(playlist, songs, function(err,response) {
-				if ( !err ) {
-					return d10.realrest.success(response, request.ctx);
-				}
-				return d10.realrest.err(423,err,request.ctx);
-			});
-		});
-	});
+    return d10.couch.d10.getDoc(request.body.playlist, (err, playlist) => {
+      if (err) {
+        return d10.realrest.err(423, err, request.ctx);
+      }
+      if (playlist.user !== request.ctx.user._id) {
+        return d10.realrest.err(403, null, request.ctx);
+      }
+      return rpl.update(playlist, songs, (err2, response) => {
+        if (!err2) {
+          return d10.realrest.success(response, request.ctx);
+        }
+        return d10.realrest.err(423, err2, request.ctx);
+      });
+    });
+  });
 
 
-	app.put("/api/plm/append/pl:id", jsonParserMiddleware, function(request,response) {
-		if ( !request.body.song ) {
-			return d10.realrest.err(427,"song parameter is empty",request.ctx);
-		}
+  app.put('/api/plm/append/pl:id', jsonParserMiddleware, (request) => {
+    if (!request.body.song) {
+      return d10.realrest.err(427, 'song parameter is empty', request.ctx);
+    }
 
-		d10.couch.d10.getDoc("pl"+request.params.id,function(err,playlist) {
-			if ( err ) {
-				return d10.realrest.err(423,err,request.ctx);
-			}
-			if ( playlist.user != request.ctx.user._id ) {
-				return d10.realrest.err(403,null,request.ctx);
-			}
-			rpl.append(playlist, request.body.song, function(err,resp) {
-				if ( !err ) {
-					return d10.realrest.success({playlist: resp.playlist, song: resp.song }, request.ctx);
-				}
-				return d10.realrest.err(423,null,request.ctx);
-			});
-		});
-	});
+    return d10.couch.d10.getDoc(`pl${request.params.id}`, (err, playlist) => {
+      if (err) {
+        return d10.realrest.err(423, err, request.ctx);
+      }
+      if (playlist.user !== request.ctx.user._id) {
+        return d10.realrest.err(403, null, request.ctx);
+      }
+      return rpl.append(playlist, request.body.song, (err2, resp) => {
+        if (!err2) {
+          return d10.realrest.success({ playlist: resp.playlist, song: resp.song }, request.ctx);
+        }
+        return d10.realrest.err(423, null, request.ctx);
+      });
+    });
+  });
 
-	app.put("/api/plm/create", jsonParserMiddleware, function(request,response) {
-		if ( !request.body.name || !request.body.name.length ) {
-			return d10.realrest.err(427,"name parameter missing",request.ctx);
-		}
-		var songs = request.body.songs || [];
-		if ( Object.prototype.toString.call(songs) !== '[object Array]' ) {
-			songs = [ songs ];
-		}
-		rpl.create(request.ctx.user._id, request.body.name, songs,
-				   function(err,playlist) {
-					   if ( err ) {
-						   d10.realrest.err(423,err,request.ctx);
-					   } else {
-						   d10.realrest.success(playlist, request.ctx);
-					   }
-				   }
-		);
-	});
+  app.put('/api/plm/create', jsonParserMiddleware, (request) => {
+    if (!request.body.name || !request.body.name.length) {
+      return d10.realrest.err(427, 'name parameter missing', request.ctx);
+    }
+    let songs = request.body.songs || [];
+    if (Object.prototype.toString.call(songs) !== '[object Array]') {
+      songs = [songs];
+    }
+    return rpl.create(request.ctx.user._id, request.body.name, songs,
+      (err, playlist) => {
+        if (err) {
+          d10.realrest.err(423, err, request.ctx);
+        } else {
+          d10.realrest.success(playlist, request.ctx);
+        }
+      });
+  });
 
-	app.put("/api/plm/rename/pl:id", jsonParserMiddleware, function(request,response) {
-		if ( !request.body.name || !request.body.name.length ) {
-			return d10.realrest.err(427,"name parameter missing",request.ctx);
-		}
-		rpl.rename(request.ctx.user._id, "pl"+request.params.id, request.body.name,
-				   function(err,playlist) {
-					   if ( err ) {
-						   d10.realrest.err(423,err,request.ctx);
-					   } else {
-						   d10.realrest.success(playlist, request.ctx);
-					   }
-				   }
-		);
-	});
+  app.put('/api/plm/rename/pl:id', jsonParserMiddleware, (request) => {
+    if (!request.body.name || !request.body.name.length) {
+      return d10.realrest.err(427, 'name parameter missing', request.ctx);
+    }
+    return rpl.rename(request.ctx.user._id, `pl${request.params.id}`, request.body.name,
+      (err, playlist) => {
+        if (err) {
+          d10.realrest.err(423, err, request.ctx);
+        } else {
+          d10.realrest.success(playlist, request.ctx);
+        }
+      });
+  });
 
-	app.delete("/api/plm/pl:id",function(request,response) {
-		var playlist_id = "pl"+request.params.id;
-		d10.couch.d10.getDoc(playlist_id,function(err, playlist) {
-			if ( err ) {
-				d10.realrest.err(423,err,request.ctx);
-			}
-			if ( playlist.user != request.ctx.user._id ) {
-				return d10.realrest.err(403,"",request.ctx);
-			}
-			d10.couch.d10.deleteDoc(playlist,function(err,resp) {
-				if ( err ) {
-					return d10.realrest.err(423,err,request.ctx);
-				}
-				d10.realrest.success(playlist, request.ctx);
-			});
-		});
-	});
+  app.delete('/api/plm/pl:id', (request) => {
+    const playlistId = `pl${request.params.id}`;
+    d10.couch.d10.getDoc(playlistId, (err, playlist) => {
+      if (err) {
+        d10.realrest.err(423, err, request.ctx);
+      }
+      if (playlist.user != request.ctx.user._id) {
+        return d10.realrest.err(403, '', request.ctx);
+      }
+      return d10.couch.d10.deleteDoc(playlist, (err2) => {
+        if (err2) {
+          return d10.realrest.err(423, err2, request.ctx);
+        }
+        return d10.realrest.success(playlist, request.ctx);
+      });
+    });
+  });
 };
