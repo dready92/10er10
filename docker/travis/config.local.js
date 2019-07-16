ae4328fc83c5d3c80a2241ba88db0d6f44a088a4
Michael Bailly <mickael.bailly@free.fr>
2019-07-16 16:43:53 +0200

    Correct files configuration path for TravisCI

---
+++ docker/travis/config.local.js
@@ -0,0 +1,13 @@
+exports.audio = {
+  tmpdir: "/fixtures/files/tmp",
+  dir: "/fixtures/files/audio",
+
+  // when the source audio file is already encoded, and not on ogg format,
+  // setting this to true will also record the original file
+  keepOriginalFile: true
+};
+
+exports.images = {
+  tmpdir: "/fixtures/files/tmp",
+  dir: "/fixtures/files/images",
+};
