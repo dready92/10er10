046ad61309c3b5909a85ac81d6f57b916b91ea6c
Michael Bailly <mickael.bailly@free.fr>
2020-05-09 12:50:40 +0200

    [feature] Add fields to API Key object

--- node/lib/api-key.js
+++ node/lib/api-key.js
@@ -3,6 +3,8 @@ class ApiKey {
     this.key = keyAsString;
     this.creation = Date.now();
     this.disabled = false;
+    this.description = null;
+    this.pairing = null;
   }
 
   active() {
@@ -13,6 +15,8 @@ class ApiKey {
     const apiKey = new ApiKey(apiKeyObject.key);
     apiKey.creation = apiKeyObject.creation;
     apiKey.disabled = apiKeyObject.disabled;
+    this.description = ('description' in apiKeyObject) ? apiKeyObject.description : null;
+    this.pairing = ('pairing' in apiKeyObject) ? apiKeyObject.pairing : null;
   }
 }
 
